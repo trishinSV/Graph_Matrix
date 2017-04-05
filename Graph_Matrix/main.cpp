c92bfe8c64ed4538e16fbd0c6f9072f68a1a8152
trishinSV <Sergey@MacBook-Pro-Sergej.local>
2017-04-05 19:46:51 +0300

    Initial Commit

---
+++ Graph_Matrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graph_Matrix
+//
+//  Created by Сергей Тришин on 05.04.17.
+//  Copyright © 2017 Сергей Тришин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
